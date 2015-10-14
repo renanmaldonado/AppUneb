cea0507885cd9ad7974476d848d4cdd59856b059
Renan Maldonado <renan.maldonado@outlook.com>
2015-10-14 10:28:03 -0400

    versão 1.75
    app da uneb

---
+++ www/includes/footer-content.php
@@ -0,0 +1,10 @@
+<div data-role="footer" data-position="fixed">
+    <div class="footer-actions">
+        <a href="tel://971568899009"><img src="images/icons/phone.png"></a>
+        <a target="_blank" href="http://www.facebook.com/riten.vagadiya"><img src="images/icons/fb.png"></a>
+        <a href="contact-us.html"><img src="images/icons/location.png"></a>
+        <div class="clear"></div>
+    </div>
+    <p class="right">&copy; New Year 2013.</p>
+    <div class="clear"></div>
+</div>
