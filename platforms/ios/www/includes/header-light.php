4db38b87e1d437a7e6e31ff88521f5b93b85b03e
Renan Maldonado <renan.maldonado@outlook.com>
2015-11-09 16:18:46 -0400

    versão 8.0
    teste de app uneb

---
+++ platforms/ios/www/includes/header-light.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html> 
+<html> 
+	<head> 
+	<title>New Year 2013</title> 
+	<meta id="extViewportMeta" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />	
+
+	<!-- Home screen icon  Mathias Bynens mathiasbynens.be/notes/touch-icons -->
+	<!-- For iPhone 4 with high-resolution Retina display: -->
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/icon.png">
+	<!-- For first-generation iPad: -->
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/icon.png">
+	<!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
+	<link rel="apple-touch-icon-precomposed" href="images/icon.png">
+	<!-- For nokia devices: -->
+	<link rel="shortcut icon" href="images/icon.png">
+
+
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/themes/default/RSVmain.min.css" />
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.1/jquery.mobile.structure-1.1.1.min.css" />
+        <link rel="stylesheet/less" href="css/style-light.css">
+
+	 <!-- fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/flexslider.css">
+	<link rel="stylesheet" href="css/photoswipe.css">
+	<link rel="stylesheet" href="css/add2home.css">
+	
+	
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/jquery.mobile-1.1.1.min.js"></script>
+        <script type="text/javascript" src="js/less-1.3.0.min.js"></script>
+        <script type="text/javascript" src="js/jquery-ui-effects.js"></script>
+
+	<script src="js/helper.js"></script>
+	<script src="js/jquery.flexslider-min.js"></script>
+	<script src="js/iphone-style-checkboxes.js"></script>
+	<script src="js/klass.min.js"></script>
+	<script src="js/code.photoswipe.jquery-3.0.5.min.js"></script>
+	<script src="js/add2home.js"></script>
+        
+	<script type="text/javascript" src="js/app.js?v=30"></script>
+</head> 
+
+<body>
+	<!-- Splash screen -->
+  	 <div id="splash"> 
+	   <img id="splash-bg" src="images/splash/splash-alternate.png" alt="splash image"/>
+	   <img id="splash-title" src="images/splash/main.png" alt="splash title" />
+	 </div> 
+  <!-- end splash screen -->
+	<div id="menu">
+		<h3><input id="search" type="text" placeholder="Search" /> </h3>
+		<ul>
+			<li class="active"><a href="./" data-transition="pop" class="contentLink">Home</a></li>
+			<li><a href="company-profile.html" data-transition="pop" class="contentLink">Company Profile</a></li>
+			<li><a href="products-services.html"data-transition="pop" class="contentLink">Products &amp; Services</a></li>
+			<li><a href="#home" class="contentLink">Portfolio</a></li>
+			<li><a href="news-events.html" data-transition="pop" class="contentLink">News &amp; Events</a></li>
+			<li><a href="gallery.html" data-transition="pop" class="contentLink">Gallery</a></li>
+			<li><a href="#home" class="contentLink">Typography</a></li>
+			<li><a href="#home" class="contentLink">Wordpress Blog</a></li>
+			<li><a href="twitter.html" data-transition="slide" class="contentLink">Twitter Feeds</a></li>
+			<li><a href="youtube.html" data-transition="pop" class="contentLink">Youtube Channel</a></li>
+			<li><a href="#home" class="contentLink">Contact Us</a></li>
+			<li><a href="about.html" data-transition="slide" class="contentLink">About</a></li>
+		</ul>
+	</div>
